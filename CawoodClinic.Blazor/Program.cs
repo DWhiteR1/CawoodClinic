166674df5607f11198ae89ac01620b6e558bc078
Dean White <Dean@Cordys.co.za>
2023-11-30 19:38:59 +0200

    Login screen changes

--- CawoodClinic.Blazor/Program.cs
+++ CawoodClinic.Blazor/Program.cs
@@ -28,10 +28,33 @@ builder.Services.AddDbContextFactory<ApplicationDbContext>(opt =>
     opt.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
+{
+    //options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedEmail = false;
+})
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CanMaintainUsers", policy => policy.RequireClaim("Permission", "CanMaintainUsers"));
+    options.AddPolicy("CanMaintainAccountDetails", policy => policy.RequireClaim("Permission", "CanMaintainAccountDetails"));
+    options.AddPolicy("CanSendAccountDetails", policy => policy.RequireClaim("Permission", "CanSendAccountDetails"));
+    options.AddPolicy("CanMaintainCompanies", policy => policy.RequireClaim("Permission", "CanMaintainCompanies"));
+    options.AddPolicy("CanViewAccountDetails", policy => policy.RequireClaim("Permission", "CanViewAccountDetails"));
+    options.AddPolicy("CanViewCompanies", policy => policy.RequireClaim("Permission", "CanViewCompanies"));
+    options.AddPolicy("CanViewSMSHistory", policy => policy.RequireClaim("Permission", "CanViewSMSHistory"));
+});
+
+
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
